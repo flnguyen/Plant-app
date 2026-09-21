7326248894b871ec3179fefca8b9074945ebed5f
felicia <flnguyen@bu.edu>
2026-09-21 06:40:00 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+// Find the "+ Add Plant" button in our HTML.
+const addPlantButton = document.querySelector(".add-button");
+
+// Listen for a tap/click on the button.
+addPlantButton.addEventListener("click", function () {
+  alert("Add Plant works!");
+});
